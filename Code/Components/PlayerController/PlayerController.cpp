e6980b30c7989632c58698f24792e1ca9f85ef5d
andres_sempai <andresquezadatoledo@gmail.com>
2023-04-21 20:31:03 -0500

    feat(PlayerController): Rotate Camera with Inputs

--- Code/Components/PlayerController/PlayerController.cpp
+++ Code/Components/PlayerController/PlayerController.cpp
@@ -6,6 +6,7 @@
 #include <CryCore/StaticInstanceList.h>
 #include <DefaultComponents/Input/InputComponent.h>
 #include <DefaultComponents/Physics/CharacterControllerComponent.h>
+#include <DefaultComponents/Cameras/CameraComponent.h>
 
 void register_player_controller(Schematyc::IEnvRegistrar& registrar)
 {
@@ -18,11 +19,23 @@ CRY_STATIC_AUTO_REGISTER_FUNCTION(&register_player_controller);
 void CPlayerController::ReflectType(Schematyc::CTypeDesc<CPlayerController>& desc)
 {
 	desc.SetGUID("{761DD699-0799-4FB8-BCDC-024ECC72919A}"_cry_guid);
-}
+	desc.AddMember(
+			&CPlayerController::rotation_pitch_min_
+		,	'pmin'
+		,	"pitchmin"
+		,	"Rotation Pitch Min"
+		,	nullptr
+		,	rotation_pitch_min
+	);
 
-CPlayerController::CPlayerController()
-{
-	
+	desc.AddMember(
+			&CPlayerController::rotation_pitch_max_
+		,	'pmax'
+		,	"pitchmax"
+		,	"Rotation Pitch Max"
+		,	nullptr
+		,	rotation_pitch_max
+	);
 }
 
 void CPlayerController::Initialize()
@@ -30,7 +43,58 @@ void CPlayerController::Initialize()
 	using namespace Cry::DefaultComponents;
 
 	input_component_ = m_pEntity->GetOrCreateComponent<CInputComponent>();
+	input_component_->RegisterAction("player", "rotate_yaw", [this](int activation_mode, float value)
+		{
+			mouse_location_delta_.x -= value;
+		});
+
+	input_component_->RegisterAction("player", "rotate_pitch", [this](int activation_mode, float value)
+		{
+			mouse_location_delta_.y -= value;
+		});
+
+	input_component_->BindAction("player", "rotate_yaw", eAID_KeyboardMouse, eKI_MouseX);
+	input_component_->BindAction("player", "rotate_pitch", eAID_KeyboardMouse, eKI_MouseY);
+
 	character_controller_component_ = m_pEntity->GetOrCreateComponent<CCharacterControllerComponent>();
 	character_controller_component_->SetTransformMatrix(Matrix34::CreateTranslationMat(Vec3{ 0.0f, 0.0f, 0.5f }));
 	character_controller_component_->Physicalize();
+
+	camera_component_ = m_pEntity->GetOrCreateComponent<CCameraComponent>();
+	camera_component_->SetTransformMatrix(Matrix34::CreateTranslationMat(Vec3{ 0.0f, 0.0f, 0.735f }));
+}
+
+Cry::Entity::EventFlags CPlayerController::GetEventMask() const
+{
+	return Cry::Entity::EEvent::Update;
+}
+
+void CPlayerController::ProcessEvent(const SEntityEvent& event)
+{
+	if (gEnv->IsEditing())
+		return;
+
+	if (!mouse_location_delta_.IsZero()) {
+		auto camera_transform = camera_component_->GetTransformMatrix();
+
+		static constexpr auto rotation_speed = 0.002f;
+		const auto rotation_limit_pitch_min = DEG2RAD(rotation_pitch_min_);
+		const auto rotation_limit_pitch_max = DEG2RAD(rotation_pitch_max_);
+
+		auto camera_ypr = CCamera::CreateAnglesYPR(Matrix33{ camera_transform });
+		camera_ypr.x += mouse_location_delta_.x * rotation_speed;
+
+		camera_ypr.y = crymath::clamp(camera_ypr.y + mouse_location_delta_.y * rotation_speed
+			,	rotation_limit_pitch_min
+			,	rotation_limit_pitch_max
+		);
+
+		camera_ypr.z = 0.0f;
+
+		camera_transform.SetRotation33(CCamera::CreateOrientationYPR(camera_ypr));
+
+		camera_component_->SetTransformMatrix(camera_transform);
+	}
+
+	mouse_location_delta_.zero();
 }
--- Code/Components/PlayerController/PlayerController.h
+++ Code/Components/PlayerController/PlayerController.h
@@ -3,17 +3,27 @@
 namespace Cry::DefaultComponents {
 	class CInputComponent;
 	class CCharacterControllerComponent;
+	class CCameraComponent;
 }
 
 class CPlayerController final : public IEntityComponent {
+	static constexpr auto rotation_pitch_min = -75.0f;
+	static constexpr auto rotation_pitch_max = 80.0f;
+
 public:
 	static void ReflectType(Schematyc::CTypeDesc<CPlayerController>& desc);
-
-	CPlayerController();
-
+	
 private:
 	void Initialize() override;
+	Cry::Entity::EventFlags GetEventMask() const override;
+	void ProcessEvent(const SEntityEvent& event) override;
+
+	float rotation_pitch_min_{ rotation_pitch_min };
+	float rotation_pitch_max_{ rotation_pitch_max };
 
 	Cry::DefaultComponents::CInputComponent* input_component_{};
 	Cry::DefaultComponents::CCharacterControllerComponent* character_controller_component_{};
+	Cry::DefaultComponents::CCameraComponent* camera_component_{};
+
+	Vec2 mouse_location_delta_{ ZERO };
 };
