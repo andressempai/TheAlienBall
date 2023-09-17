10a8bb14700fe0f1f9865538c65011af5cf8e92c
andres_sempai <andresquezadatoledo@gmail.com>
2023-09-16 21:07:54 -0500

    Spring and Jump Logic

--- Code/Components/PlayerController/PlayerController.cpp
+++ Code/Components/PlayerController/PlayerController.cpp
@@ -7,7 +7,6 @@
 #include <DefaultComponents/Input/InputComponent.h>
 #include <DefaultComponents/Physics/CharacterControllerComponent.h>
 #include <DefaultComponents/Cameras/CameraComponent.h>
-#include <CryPhysics/physinterface.h>
 
 void register_player_controller(Schematyc::IEnvRegistrar& registrar)
 {
@@ -22,74 +21,29 @@ void CPlayerController::ReflectType(Schematyc::CTypeDesc<CPlayerController>& des
 	desc.SetGUID("{761DD699-0799-4FB8-BCDC-024ECC72919A}"_cry_guid);
 	desc.AddMember(
 			&CPlayerController::min_walk_speed_
-		,	'misp'
+		,	'mwsp'
 		,	"minwalkspeed"
 		,	"Min Walk Speed"
-		,	"Min Speed when the Player is Walking"
+		,	"Speed when player walking"
 		,	min_walk_speed
 	);
 
 	desc.AddMember(
 			&CPlayerController::max_walk_speed_
-		,	'masp'
+		,	'maws'
 		,	"maxwalkspeed"
 		,	"Max Walk Speed"
-		,	"Max Speed when the Player is Walking"
+		,	"Speed when the player Sprint"
 		,	max_walk_speed
 	);
 
 	desc.AddMember(
-			&CPlayerController::max_jump_speed_
-		,	'majp'
-		,	"maxjumpspeed"
-		,	"Max Jump Speed"
-		,	"Max Speed when the Player Jump"
-		,	max_jump_speed
-	);
-
-	desc.AddMember(
-			&CPlayerController::capsule_stand_height_
-		,	'cshe'
-		,	"capsulestandheight"
-		,	"Capsule Stand Height"
-		,	"Height of the Capsule when the Player is Stand"
-		,	capsule_stand_height
-	);
-
-	desc.AddMember(
-			&CPlayerController::capsule_crouch_height_
-		,	'cche'
-		,	"capsulecrouchheight"
-		,	"Capsule Crouch Height"
-		,	"Height of the Capsule when the Player is Crouch"
-		,	capsule_crouch_height
-	);
-
-	desc.AddMember(
-			&CPlayerController::capsule_height_offset_
-		,	'chof'
-		,	"capsuleheightoffset"
-		,	"Capsule Height Offset"
-		,	"Offset of the Capule Height from Ground"
-		,	capsule_height_offset
-	);
-
-	desc.AddMember(
-			&CPlayerController::camera_stand_height_
-		,	'cash'
-		,	"camerastandheight"
-		,	"Camera Stand Height"
-		,	"Camera Height when the Player is Stand"
-		,	camera_stand_height
-	);
-
-	desc.AddMember(
-			&CPlayerController::camera_crouch_height_
-		,	'cach'
-		,	"cameracrouchheight"
-		,	"Camera Crouch Height"
-		,	"Camera Height when the Player is Crouch"
-		,	camera_crouch_height
+			&CPlayerController::jump_speed_
+		,	'jspe'
+		,	"jumpspeed"
+		,	"Jump Speed"
+		,	"Speed when the player Jump"
+		,	jump_speed
 	);
 
 	desc.AddMember(
@@ -129,43 +83,37 @@ void CPlayerController::Initialize()
 	input_component_->RegisterAction("player", "moveforward", [this](int activation_mode, float value)
 		{
 			if (activation_mode == eAAM_OnHold)
-				states_flags_.set(move_forward);
+				state_flags_.set(move_forward);
 		});
 
 	input_component_->RegisterAction("player", "moveback", [this](int activation_mode, float value)
 		{
 			if (activation_mode == eAAM_OnHold)
-				states_flags_.set(move_back);
+				state_flags_.set(move_back);
 		});
 
 	input_component_->RegisterAction("player", "moveright", [this](int activation_mode, float value)
 		{
 			if (activation_mode == eAAM_OnHold)
-				states_flags_.set(move_right);
+				state_flags_.set(move_right);
 		});
 
 	input_component_->RegisterAction("player", "moveleft", [this](int activation_mode, float value)
 		{
 			if (activation_mode == eAAM_OnHold)
-				states_flags_.set(move_left);
+				state_flags_.set(move_left);
 		});
 
 	input_component_->RegisterAction("player", "boost", [this](int activation_mode, float value)
 		{
 			if (activation_mode == eAAM_OnHold)
-				states_flags_.set(boost);
+				state_flags_.set(boost);
 		});
 
-	input_component_->RegisterAction("player", "jump", [this](int activation_mode, float value)
+	input_component_->RegisterAction("player", "moveup", [this](int activation_mode, float value)
 		{
 			if (activation_mode == eAAM_OnPress)
-				states_flags_.set(jump);
-		});
-
-	input_component_->RegisterAction("player", "crouch", [this](int activation_mode, float value)
-		{
-			if (activation_mode == eAAM_OnHold)
-				states_flags_.set(crouch);
+				state_flags_.set(jump);
 		});
 
 	input_component_->BindAction("player", "rotate_yaw", eAID_KeyboardMouse, eKI_MouseX);
@@ -175,22 +123,14 @@ void CPlayerController::Initialize()
 	input_component_->BindAction("player", "moveright", eAID_KeyboardMouse, eKI_D);
 	input_component_->BindAction("player", "moveleft", eAID_KeyboardMouse, eKI_A);
 	input_component_->BindAction("player", "boost", eAID_KeyboardMouse, eKI_LShift);
-	input_component_->BindAction("player", "jump", eAID_KeyboardMouse, eKI_Space);
-	input_component_->BindAction("player", "crouch", eAID_KeyboardMouse, eKI_LCtrl);
+	input_component_->BindAction("player", "moveup", eAID_KeyboardMouse, eKI_Space);
 
 	character_controller_component_ = m_pEntity->GetOrCreateComponent<CCharacterControllerComponent>();
-	character_controller_component_->GetPhysicsParameters().m_height = capsule_stand_height;
-
-	const auto character_radius = character_controller_component_->GetPhysicsParameters().m_radius * 0.50f;
-	const auto character_height = character_controller_component_->GetPhysicsParameters().m_height * 0.25f;
-	const auto character_offset = character_radius + character_height + capsule_height_offset;
-	const auto character_transform = Matrix34::CreateTranslationMat({ 0.0f, 0.0f, character_offset });
-
-	character_controller_component_->SetTransformMatrix(character_transform);
+	character_controller_component_->SetTransformMatrix(Matrix34::CreateTranslationMat(Vec3{ 0.0f, 0.0f, 0.5f }));
 	character_controller_component_->Physicalize();
 
 	camera_component_ = m_pEntity->GetOrCreateComponent<CCameraComponent>();
-	camera_component_->SetTransformMatrix(Matrix34::CreateTranslationMat(Vec3{ 0.0f, 0.0f, camera_stand_height_ }));
+	camera_component_->SetTransformMatrix(Matrix34::CreateTranslationMat(Vec3{ 0.0f, 0.0f, 0.735f }));
 }
 
 Cry::Entity::EventFlags CPlayerController::GetEventMask() const
@@ -202,29 +142,31 @@ void CPlayerController::ProcessEvent(const SEntityEvent& event)
 {
 	if (gEnv->IsEditing())
 		return;
-	
-	const auto frame_time = event.fParam[0];
 
-	auto velocity = Vec3{ ZERO };
-	
-	if (states_flags_.test(move_forward))
-		velocity.y += 1.0f;
-	if (states_flags_.test(move_back))
-		velocity.y -= 1.0f;
-	if (states_flags_.test(move_right))
-		velocity.x += 1.0f;
-	if (states_flags_.test(move_left))
-		velocity.x -= 1.0f;
+	auto move_velocity = Vec3{ ZERO };
 
-	velocity.Normalize();
+	if (state_flags_.test(move_forward))
+		move_velocity.y += 1.0f;
+	if (state_flags_.test(move_back))
+		move_velocity.y -= 1.0f;
+	if (state_flags_.test(move_right))
+		move_velocity.x += 1.0f;
+	if (state_flags_.test(move_left))
+		move_velocity.x -= 1.0f;
 
-	const auto desired_acceleration = states_flags_.test(boost) ? max_walk_speed_ : min_walk_speed_;
+	move_velocity.Normalize();
 
-	walk_acceleration_ = Lerp(walk_acceleration_, desired_acceleration, 7.55f * frame_time);
+	if (state_flags_.test(boost))
+		move_velocity = move_velocity.scale(max_walk_speed_);
 
 	const auto camera_rotation = Quat{ camera_component_->GetTransformMatrix() };
-	const auto yaw_rotation = Quat::CreateRotationZ(camera_rotation.GetRotZ());
-	character_controller_component_->SetVelocity(yaw_rotation * velocity * walk_acceleration_);
+	const auto movement_vector = move_velocity * min_walk_speed_;
+	character_controller_component_->SetVelocity(Quat::CreateRotationZ(camera_rotation.GetRotZ()) * movement_vector);
+
+	if (state_flags_.test(jump)) {
+		if (character_controller_component_->IsOnGround())
+			character_controller_component_->AddVelocity({ 0.0f, 0.0f, jump_speed_ });
+	}
 
 	if (!mouse_location_delta_.IsZero()) {
 		auto camera_transform = camera_component_->GetTransformMatrix();
@@ -248,48 +190,6 @@ void CPlayerController::ProcessEvent(const SEntityEvent& event)
 		camera_component_->SetTransformMatrix(camera_transform);
 	}
 
-	const auto current_stance = states_flags_.test(crouch) ? EPlayer_Stance::Crouching : EPlayer_Stance::Standing;
-
-	if (desired_stance_ != current_stance) {
-		desired_stance_ = current_stance;
-
-		auto dimensions = pe_player_dimensions{};
-		m_pEntity->GetPhysics()->GetParams(&dimensions);
-
-		auto character_height = 0.0f;
-
-		switch(desired_stance_)
-		{
-		case EPlayer_Stance::Standing:
-		{
-			character_height = capsule_stand_height;
-		}
-		break;
-
-		case EPlayer_Stance::Crouching:
-		{
-			character_height = capsule_crouch_height;
-		}
-		break;
-		}
-
-		dimensions.sizeCollider.z = character_height * 0.25f;
-		dimensions.heightCollider = dimensions.sizeCollider.x + dimensions.sizeCollider.z + capsule_height_offset_;
-		m_pEntity->GetPhysicalEntity()->SetParams(&dimensions);
-	}
-
-	const auto eyes_height = states_flags_.test(crouch) ? camera_crouch_height_ : camera_stand_height_;
-
-	auto camera_transform = camera_component_->GetTransformMatrix();
-	auto camera_position = camera_transform.GetColumn3();
-	camera_position.z = Lerp(camera_position.z, eyes_height, 10.0f * frame_time);
-
-	camera_transform.SetTranslation(camera_position);
-	camera_component_->SetTransformMatrix(camera_transform);
-
-	if (states_flags_.test(jump) && character_controller_component_->IsOnGround())
-		character_controller_component_->AddVelocity({ 0.0f, 0.0f, max_jump_speed_ });
-
+	state_flags_.reset();
 	mouse_location_delta_.zero();
-	states_flags_.reset();
 }
--- Code/Components/PlayerController/PlayerController.h
+++ Code/Components/PlayerController/PlayerController.h
@@ -9,31 +9,23 @@ namespace Cry::DefaultComponents {
 }
 
 class CPlayerController final : public IEntityComponent {
-	static constexpr auto min_walk_speed = 2.35f;
-	static constexpr auto max_walk_speed = 4.35f;
-	static constexpr auto max_jump_speed = 5.35f;
-	static constexpr auto capsule_stand_height = 1.77f;
-	static constexpr auto capsule_crouch_height = 0.83f;
-	static constexpr auto capsule_height_offset = 0.02f;
-	static constexpr auto camera_stand_height = 1.57f;
-	static constexpr auto camera_crouch_height = 0.63f;
+	static constexpr auto min_walk_speed = 2.5f;
+	static constexpr auto max_walk_speed = 4.5f;
+	static constexpr auto jump_speed = 5.7f;
 	static constexpr auto rotation_pitch_min = -75.0f;
 	static constexpr auto rotation_pitch_max = 80.0f;
 
 	enum {
-			move_forward = 0
+			idle = 0
+		,	move_forward
 		,	move_back
 		,	move_right
 		,	move_left
 		,	boost
 		,	jump
 		,	crouch
+		,	interact
 		,	total_states
-	};
-
-	enum struct EPlayer_Stance {
-			Standing
-		,	Crouching
 	};
 
 public:
@@ -46,12 +38,7 @@ private:
 
 	float min_walk_speed_{ min_walk_speed };
 	float max_walk_speed_{ max_walk_speed };
-	float max_jump_speed_{ max_jump_speed };
-	float capsule_stand_height_{ capsule_stand_height };
-	float capsule_crouch_height_{ capsule_crouch_height };
-	float capsule_height_offset_{ capsule_height_offset };
-	float camera_stand_height_{ camera_stand_height };
-	float camera_crouch_height_{ camera_crouch_height };
+	float jump_speed_{ jump_speed };
 	float rotation_pitch_min_{ rotation_pitch_min };
 	float rotation_pitch_max_{ rotation_pitch_max };
 
@@ -60,8 +47,6 @@ private:
 	Cry::DefaultComponents::CCameraComponent* camera_component_{};
 
 	Vec2 mouse_location_delta_{ ZERO };
-	std::bitset<total_states> states_flags_{};
 
-	float walk_acceleration_{ min_walk_speed_ };
-	EPlayer_Stance desired_stance_{ EPlayer_Stance::Standing };
+	std::bitset<total_states> state_flags_{};
 };
